279e7024e2f5cffdc3f796c11d3b721b6d4a48ff
kevinsblog <zhugy92@163.com>
2019-06-14 13:15:33 +0800

    update dfs

--- dfs.cpp
+++ dfs.cpp
@@ -58,3 +58,73 @@ vector<vector<int>> combine(int n, int k) {
 //     }
 //     return move(ans);
 // }
+
+//79. Word Search
+bool existDFS(const vector<vector<char>> &board, const string &word, vector<pair<int, int>> &path){
+    if(path.size() == word.size()){
+        return true;
+    }
+
+    int offset[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    for(int i = 0; i < 4; i++){
+        int x = path.back().first + offset[i][0],
+            y = path.back().second + offset[i][1];
+        if(x < 0 || x >= board.size() || y < 0 || y >= board.at(0).size()){
+            continue;
+        }
+        if(board[x][y] != word[path.size()]){
+            continue;   
+        }
+        auto pos = pair<int, int>(x, y);
+        if(find(path.begin(), path.end(), pos) != path.end()){
+            continue;
+        }
+        path.push_back(pos);
+        if(existDFS(board, word, path)){
+            return true;
+        }
+        path.pop_back();
+    }
+    
+    return false;
+}
+bool exist(vector<vector<char>> &board, string word){
+    unordered_map<char, vector<pair<int, int>>> start_at;
+    for(int i = 0; i < board.size(); i++){
+        for(int j = 0; j < board[i].size(); j++){
+            start_at[board[i][j]].push_back(pair<int, int>(i, j));
+        }
+    }
+
+    for(auto & start : start_at[*word.begin()]){
+        vector<pair<int, int>> path{start};
+        if(existDFS(board, word, path)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//212. Word Search II
+vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    unordered_map<char, vector<pair<int, int>>> start_at;
+    for(int i = 0; i < board.size(); i++){
+        for(int j = 0; j < board[i].size(); j++){
+            start_at[board[i][j]].push_back(pair<int, int>(i, j));
+        }
+    }
+
+    vector<string> ans;
+    for(auto & word : words){
+        for(auto & start : start_at[*word.begin()]){
+            vector<pair<int, int>> path{start};
+            if(existDFS(board, word, path)){
+                if(find(ans.begin(), ans.end(), word) == ans.end())
+                    ans.push_back(word);
+            }
+        }        
+    }
+
+    return move(ans);
+}
